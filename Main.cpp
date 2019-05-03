7d33e1869c80cd7a096b2c57dd2343558d9e9e5d
zago1 <igorzagomassaro@gmail.com>
2019-05-03 17:40:22 -0300

    Ultimo commit

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
   {
   try
      {
-     ConcessionariaModerna * cliente = new ConcessionariaModerna("www.autoftveiculos.com.br","Auto-FT_Veiculos", "@Auto-FT", "+55 19 2113-3306","Auto-FT Veiculos S.A.", "R. Paschoal Marmo, 1888", "Limeira", "SP", "+55 19 2113-3306");
+     ConcessionariaModerna * cliente = new ConcessionariaModerna("www.autoftveiculos.com.br","Auto-FT Veiculos", "@auto_ft", "+55 19 2113-3306","Auto-FT Veiculos LTDA", "R. Paschoal Marmo, 1888", "Limeira", "SP", "+55 19 2113-3306");
      Sistema * programa = new Sistema(cliente);
      programa->processa();
      delete cliente;
